9b8c9c11043ba43abfa973b995d6ab1d68ac86d7
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-07-15 17:33:08 -0300

    Explorando a Criação de Datagrames

--- manip_de_dados/3_intro_pandas.py
+++ manip_de_dados/3_intro_pandas.py
@@ -1,5 +1,6 @@
 # aula 1 - intro pandas
 import pandas as pd
+import numpy as np
 
 series = pd.Series([10, 20, 30, 40])
 
@@ -14,3 +15,25 @@ print(df)
 print(df.info())
 
 print(df.describe())
+
+# aula 2 - criação de dataframes
+
+df = pd.DataFrame([[1, 'A'], [2, 'B'], [3, 'C']], columns=['ID', 'Letra'])
+
+print(df)
+
+array = np.array([[10, 20], [30, 40]])
+
+df = pd.DataFrame(array, columns=['Col1', 'Col2'])
+
+print(df)
+
+df = pd.read_csv("manip_de_dados/dados.csv")
+
+print(df)
+
+df = pd.read_csv('manip_de_dados/teste.csv')
+
+df = df.fillna("Valor Padrão").infer_objects(copy=False)
+
+print(df)
