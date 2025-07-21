9ed48ea6bb8df3557734ceaa2207e55de35272d2
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-07-21 12:22:14 -0300

    Importação de Dados com Pandas

--- manip_de_dados/4_avancando_pandas.py
+++ manip_de_dados/4_avancando_pandas.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sqlalchemy import create_engine
 
 # aula 1 - filtragem
 
@@ -37,3 +38,40 @@ print(grupo.sum())
 print(grupo.agg({'Valor': ['mean', 'max']}))
 
 print(grupo['Valor'].count())
+
+# aula 3 - lendo diferentes tipos de dados
+
+try:
+    df = pd.read_csv("manip_de_dados/dados.csv")
+    print(df.head())
+except FileNotFoundError:
+    print("Erro ao ler dados.csv")
+
+try:
+    df_excel = pd.read_excel('manip_de_dados/dados.xlsx')
+    print(df_excel.head())
+except FileNotFoundError:
+    print("erro ao ler dados.xlsx")
+
+try:
+    df_jason = pd.read_json('manip_de_dados/dados.json')
+    print(df_jason.head())
+except FileNotFoundError:
+    print('erro ao ler dados.json')
+
+engine = create_engine("sqlite:///manip_de_dados/dados.dados.db")
+
+data = {
+    'ID': [1, 2, 3],
+    'Nome': ['Ana', 'João', 'Paulo'],
+    'Idade': [20, 30, 40],
+    'Cidade': ['São Paulo', 'Floripa', 'Rio de Janeiro']
+}
+
+pd.DataFrame(data).to_sql('tabela', con=engine, if_exists='replace', index=False)
+
+try:
+    df_sql = pd.read_sql('SELECT * FROM tabela', con=engine)
+    print(df_sql.head())
+except Exception as e:
+    print(e)
